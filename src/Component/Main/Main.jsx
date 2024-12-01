39ba9a1431e4206e6efcf6c8d2bc2ef9ec1c760a
Garvyadav888 <143431703+Garvyadav888@users.noreply.github.com>
2024-12-01 13:19:17 +0530

    s

--- src/Component/Main/Main.jsx
+++ src/Component/Main/Main.jsx
@@ -1,110 +1,146 @@
-import React, { useContext, useState } from 'react'
-import '../Main/Main2.css'
-import { FaRegUserCircle } from "react-icons/fa";
-import { CiCompass1 } from "react-icons/ci";
-import { FaRegLightbulb } from "react-icons/fa6";
-import { FaRegFileCode } from "react-icons/fa6";
-import { RiMessage2Line } from "react-icons/ri";
-import { FcGallery } from "react-icons/fc";
-import { FaMicrophoneAlt } from "react-icons/fa";
-import { BsSend } from "react-icons/bs";
+/** @format */
+
+import React, { useContext, useState } from 'react';
+import '../Main/Main2.css';
+import { FaRegUserCircle } from 'react-icons/fa';
+import { CiCompass1 } from 'react-icons/ci';
+import { FaRegLightbulb } from 'react-icons/fa6';
+import { FaRegFileCode } from 'react-icons/fa6';
+import { RiMessage2Line } from 'react-icons/ri';
+import { FcGallery } from 'react-icons/fc';
+import { FaMicrophoneAlt } from 'react-icons/fa';
+import { BsSend } from 'react-icons/bs';
 import { Context } from '../../Context/Context';
-import gemini from "../../assets/gemini.png"
+import gemini from '../../assets/gemini.png';
 
 function Main() {
-const {onSent,recentprompt,showresult,loading,resultdata,newchat,setinput,setlodaing,setresultdata,privous,setshowresult , input } = useContext(Context)
- const handleChange=(text)=>{
-setinput(text)
- }
- const [Mic , setMic ] = useState(false)
-return (
-    <div className='main'>
-        <div className="nav">
-            <p onClick={()=>{
-          newchat()
-      }}>ChatBot</p>
-            <FaRegUserCircle className='img' />
-        </div>
-        <div className="main-container">
-            {
-                !showresult ?
-                <>
-                <div className="greet">
-                <p>
-                    <span>Hello, User </span>
-                    <p>How can i help you today </p>
-                </p>
-            </div>
-            <div className="cards">
-                <div className="card" onClick={()=>{
-                        setinput("Suggest Beautifut places")
-                    }} >
-                    <p>Suggest Beautifut places</p>
-                    <CiCompass1 className='img' />
-                </div>
-                <div className="card" onClick={()=>{
-                        setinput("Briefly summarize this Concepts : Urban Planning")
-                    }}>
-                    <p>Briefly summarize this Concepts : Urban Planning</p>
-                    <FaRegLightbulb  className='img' />
-                </div>
-                <div className="card"  onClick={()=>{
-                        setinput("Brainstrom team bonding activites for our work retreat")
-                    }}>
-                    <p>Brainstrom team bonding activites for our work retreat</p>
-                    <RiMessage2Line  className='img'  />
-                </div>
-                <div className="card" onClick={()=>{
-                        setinput("Improve the readability of the following code ")
-                    }}>
-                    <p>Improve the readability of the following code </p>
-                    <FaRegFileCode   className='img' />
-                </div>
-            </div>
-                </>:
-                
-                <div className="result">
-                    <div className="result-title">
-                    <FaRegUserCircle className='img' /> 
-                    <p>{recentprompt}</p>
-                    </div>
-                    <div className="result-data">
-                    <img src={gemini}alt="" className={`img ${loading ? " animation" : ""}`} />
-                    {!loading ?
-                    <p dangerouslySetInnerHTML={{__html:resultdata}}></p>
-                    :
-                    <div className="loader">
-                        <hr />
-                        <hr />
-                        <hr />
-                    </div>
-}
-                    </div>
-                </div>
-            }
-            
-            <div className="main-bottom">
-                <div className="searchbox">
-                    <input type="text" onChange={(e)=>{
-                       handleChange(e.target.value)
-                    }} value={input} placeholder='Enter prompt here ' />
-                    <div className="">
-                 
-                    <FaMicrophoneAlt className='img' onClick={()=>{
-                        setMic(!Mic)
-                    }} /> 
-                    <BsSend className='img' onClick={()=>{
-                        onSent()
-                    }} />
-                    </div>
-                </div>
-                    <p className="bottom-info">
-                        <p>dolores aut sunt</p>
-                    </p>
-            </div>
-        </div>
-    </div>
-  )
+	const {
+		onSent,
+		recentprompt,
+		showresult,
+		loading,
+		resultdata,
+		newchat,
+		setinput,
+		setlodaing,
+		setresultdata,
+		privous,
+		setshowresult,
+		input,
+	} = useContext(Context);
+	const handleChange = (text) => {
+		setinput(text);
+	};
+
+	return (
+		<div className='main'>
+			<div className='nav'>
+				<p
+					style={{ cursor: 'pointer' }}
+					onClick={() => {
+						newchat();
+					}}>
+					ChatBot
+				</p>
+				<FaRegUserCircle className='img' />
+			</div>
+			<div className='main-container'>
+				{!showresult ? (
+					<>
+						<div className='greet'>
+							<p>
+								<span>Hello, User </span>
+								<p>How can i help you today </p>
+							</p>
+						</div>
+						<div className='cards'>
+							<div
+								className='card'
+								onClick={() => {
+									setinput('Suggest Beautifut places');
+								}}>
+								<p>Suggest Beautifut places</p>
+								<CiCompass1 className='img' />
+							</div>
+							<div
+								className='card'
+								onClick={() => {
+									setinput('Briefly summarize this Concepts : Urban Planning');
+								}}>
+								<p>Briefly summarize this Concepts : Urban Planning</p>
+								<FaRegLightbulb className='img' />
+							</div>
+							<div
+								className='card'
+								onClick={() => {
+									setinput(
+										'Brainstrom team bonding activites for our work retreat'
+									);
+								}}>
+								<p>Brainstrom team bonding activites for our work retreat</p>
+								<RiMessage2Line className='img' />
+							</div>
+							<div
+								className='card'
+								onClick={() => {
+									setinput('Improve the readability of the following code ');
+								}}>
+								<p>Improve the readability of the following code </p>
+								<FaRegFileCode className='img' />
+							</div>
+						</div>
+					</>
+				) : (
+					<div className='result'>
+						<div className='result-title'>
+							<FaRegUserCircle className='img' />
+							<p>{recentprompt}</p>
+						</div>
+						<div className='result-data'>
+							<img
+								src={gemini}
+								alt=''
+								className={`img ${loading ? ' animation' : ''}`}
+							/>
+							{!loading ? (
+								<p dangerouslySetInnerHTML={{ __html: resultdata }}></p>
+							) : (
+								<div className='loader'>
+									<hr />
+									<hr />
+									<hr />
+								</div>
+							)}
+						</div>
+					</div>
+				)}
+
+				<div className='main-bottom'>
+					<div className='searchbox'>
+						<input
+							type='text'
+							onChange={(e) => {
+								handleChange(e.target.value);
+							}}
+							value={input}
+							placeholder='Enter prompt here '
+						/>
+						<div className=''>
+							<BsSend
+								className='img'
+								onClick={() => {
+									onSent();
+								}}
+							/>
+						</div>
+					</div>
+					<p className='bottom-info'>
+						<p>dolores aut sunt</p>
+					</p>
+				</div>
+			</div>
+		</div>
+	);
 }
 
-export default Main
+export default Main;
--- src/Component/Sidebar/Sidebar.jsx
+++ src/Component/Sidebar/Sidebar.jsx
@@ -1,78 +1,76 @@
-import React, { useState } from 'react'
-import  '../Sidebar/Sidebar.css'
-import gemini from "../../assets/gemini.png"
-import { IoIosAdd } from "react-icons/io";
-import {FiAlignJustify} from "react-icons/fi"
-import { RiMessage2Line } from "react-icons/ri";
-import { FaQuestion } from "react-icons/fa6";
-import { FaHistory } from "react-icons/fa";
-import { IoMdSettings } from "react-icons/io";
+/** @format */
+
+import React, { useState } from 'react';
+import '../Sidebar/Sidebar.css';
+import gemini from '../../assets/gemini.png';
+import { IoIosAdd } from 'react-icons/io';
+import { FiAlignJustify } from 'react-icons/fi';
+import { RiMessage2Line } from 'react-icons/ri';
+import { FaQuestion } from 'react-icons/fa6';
+import { FaHistory } from 'react-icons/fa';
+import { IoMdSettings } from 'react-icons/io';
 import { useContext } from 'react';
 import { Context } from '../../Context/Context.jsx';
 
-
-
-
 function Sidebar() {
-  const [extended , setextexnded]= useState(false); 
-const {onSent, privous ,setrecentprompt ,setinput, newchat } = useContext(Context)
-const loadprompt = async(prompt)=>{
-  // setrecentprompt(prompt)
-await onSent(prompt)
+	const [extended, setextexnded] = useState(false);
+	const { onSent, privous, setrecentprompt, setinput, newchat } =
+		useContext(Context);
+	const loadprompt = async (prompt) => {
+		await onSent(prompt);
+	};
+
+	return (
+		<div className={`Sidebar`}>
+			<div className='top'>
+				<FiAlignJustify
+					className='menu img'
+					onClick={() => {
+						setextexnded(!extended);
+					}}
+				/>
+				<div
+					onClick={() => {
+						newchat();
+					}}
+					className='new-chat'>
+					<IoIosAdd className='img' />
+					{extended ? <p>New Chat</p> : null}
+				</div>
+				{extended ? (
+					<div className='recent'>
+						<p className='recent-title'>Recent</p>
+						{privous.map((item, i) => {
+							return (
+								<div
+									onClick={() => {
+										loadprompt(item);
+									}}
+									className='recent-Entry'>
+									<RiMessage2Line className='img' />
+									{extended ? <p key={i}>{item.slice(0, 18)}...</p> : null}
+								</div>
+							);
+						})}
+					</div>
+				) : null}
+			</div>
+			<div className='bottom'>
+				{/* <div className='bottom-item recent-Entry '>
+					<FaQuestion className='img' />
+					{extended ? <p>Help</p> : null}
+				</div>
+				<div className='bottom-item recent-Entry '>
+					<FaHistory className='img' />
+					{extended ? <p>Your Acivity </p> : null}
+				</div>
+				<div className='bottom-item recent-Entry '>
+					<IoMdSettings className='img' />
+					{extended ? <p>Settings</p> : null}
+				</div> */}
+			</div>
+		</div>
+	);
 }
 
-
-  return (
-    <div className={`Sidebar`}>
-      <div className="top">
-      <FiAlignJustify  className='menu img' onClick={()=>{
-          
-          setextexnded(!extended)
-      }} />
-      <div onClick={()=>{
-          newchat()
-      }} className="new-chat"  >
-      <IoIosAdd className='img'  />
-          {extended ?<p>New Chat</p> : null }
-      </div>
-      {extended? 
-        <div className="recent">
-        <p className="recent-title">Recent</p>
-        {privous.map((item ,i )=>{
-          return(
-<div onClick={()=>{
-  loadprompt(item)
-}} className="recent-Entry">
-        <RiMessage2Line className='img' />
-        {extended ?<p  key={i}>{item.slice(0,18)}...</p> : null }
-        
-        </div>
-          )
-        })}
-        
-    </div>:null}
-    
-      </div>
-      <div className="bottom">
-      <div className="bottom-item recent-Entry ">
-      <FaQuestion className='img' />
-      {extended ?<p>Help</p> : null }
-      
-      </div>
-      <div className="bottom-item recent-Entry ">
-      <FaHistory className='img' />
-      {extended ? <p>Your Acivity </p> : null }
-     
-      </div>
-      <div className="bottom-item recent-Entry ">
-      <IoMdSettings className='img' />
-      {extended ? <p>Settings</p>: null }
-     
-      </div>
-
-      </div>
-    </div>
-  )
-}
-
-export default Sidebar
+export default Sidebar;
